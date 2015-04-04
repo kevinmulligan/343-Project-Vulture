75027b4bcb58ae036a82964f7597b51e4a70930a
EC2 Default User <ec2-user@ip-172-31-19-25.ec2.internal>
2015-04-04 20:33:52 +0000

    Added data initialization

---
+++ Backend/SQL/Vulture_Data_Initialization.sql
@@ -0,0 +1,68 @@
+USE `Vulture` ;
+
+insert into Accent (Name) values ('English');
+insert into Accent (Name) values ('Irish');
+insert into Accent (Name) values ('Canadian');
+insert into Accent (Name) values ('German');
+insert into Accent (Name) values ('Chinese');
+insert into Accent (Name) values ('Japanese');
+insert into Accent (Name) values ('Scottish');
+insert into Accent (Name) values ('Swedish');
+insert into Accent (Name) values ('French');
+
+insert into Sexuality (Name) values ('Straight');
+insert into Sexuality (Name) values ('Gay');
+insert into Sexuality (Name) values ('Bisexual');
+insert into Sexuality (Name) values ('Asexual');
+insert into Sexuality (Name) values ('Demisexual');
+insert into Sexuality (Name) values ('Heteroflexible');
+insert into Sexuality (Name) values ('Homoflexible');
+insert into Sexuality (Name) values ('Lesbian');
+insert into Sexuality (Name) values ('Pansexual');
+insert into Sexuality (Name) values ('Queer');
+insert into Sexuality (Name) values ('Questioning');
+insert into Sexuality (Name) values ('Sapiosexual');
+
+insert into Gender (Name) values ('Woman');
+insert into Gender (Name) values ('Man');
+insert into Gender (Name) values ('Agender');
+insert into Gender (Name) values ('Androgynous');
+insert into Gender (Name) values ('Bigender');
+insert into Gender (Name) values ('Cis Man');
+insert into Gender (Name) values ('Cis Woman');
+insert into Gender (Name) values ('Genderfluid');
+insert into Gender (Name) values ('Genderqueer');
+insert into Gender (Name) values ('Gender Nonconforming');
+insert into Gender (Name) values ('Hijra');
+insert into Gender (Name) values ('Intersex');
+insert into Gender (Name) values ('Non-binary');
+insert into Gender (Name) values ('Other');
+insert into Gender (Name) values ('Pangender');
+insert into Gender (Name) values ('Transfeminine');
+insert into Gender (Name) values ('Transgender');
+insert into Gender (Name) values ('Transmasculine');
+insert into Gender (Name) values ('Transsexual');
+insert into Gender (Name) values ('Trans Man');
+insert into Gender (Name) values ('Trans Woman');
+insert into Gender (Name) values ('Two Spirit');
+
+insert into HairColor (Name) values ('Blonde');
+insert into HairColor (Name) values ('Brunette');
+insert into HairColor (Name) values ('Red');
+insert into HairColor (Name) values ('Black');
+insert into HairColor (Name) values ('Dirty Blonde');
+insert into HairColor (Name) values ('Dyed');
+insert into HairColor (Name) values ('Bald');
+insert into HairColor (Name) values ('Other');
+
+insert into Subject (Name) values ('Math');
+insert into Subject (Name) values ('Science');
+insert into Subject (Name) values ('Social Studies');
+insert into Subject (Name) values ('Gym');
+insert into Subject (Name) values ('Music');
+insert into Subject (Name) values ('Art');
+insert into Subject (Name) values ('Foreign Language');
+insert into Subject (Name) values ('Computers');
+insert into Subject (Name) values ('Psychology');
+insert into Subject (Name) values ('English');
+
