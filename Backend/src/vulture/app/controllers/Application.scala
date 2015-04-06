9c2b28457e888acd55d622967aed42c887e50582
Matt Smicinski <mjs7769@rit.edu>
2015-04-05 20:11:21 -0700

    Added login, it's the most secure code I've ever written

--- Backend/src/vulture/app/controllers/Application.scala
+++ Backend/src/vulture/app/controllers/Application.scala
@@ -5,21 +5,23 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.db._
+import play.api.libs._
 
 object Application extends Controller {
 
   def index = Action {
-    var outString = "Accents are "
+    var outString = "Passwords are "
     val conn = DB.getConnection()
     try {
       val stmt = conn.createStatement
-      val rs = stmt.executeQuery("SELECT Name from Accent")
+      val rs = stmt.executeQuery("SELECT Password from Users where Username='tswift'")
       while (rs.next()) {
-        outString += rs.getString("Name") + " "
+        outString += rs.getString("Password") + " "
       }
     } finally {
       conn.close()
     }
+    outString += Codecs.sha1("dickbutt")
     Ok(outString)
   }
 
